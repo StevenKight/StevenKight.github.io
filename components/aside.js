9072d7fce600e1fd7423f57609c8ce6f77d3d1f8
StevenKight <sgk0711@gmail.com>
2022-04-29 15:29:52 -0400

    Aside change

--- components/aside.js
+++ components/aside.js
@@ -15,7 +15,7 @@ app.component('aside-component', {
         <div id="classes" style="text-align: center;">
             <h4 style="margin-bottom: 1em;">Fall 2022 Class Schedule:</h4>
             <h5 style="color: white;" v-for="item in classes">
-                {{ item.name }} taught by {{ item.instructer }} at {{ item.time }} on {{ item.days }}
+                {{ item.name }} at {{ item.time }} on {{ item.days }} with {{ item.instructer }}
             </h5>
         </div>
         <div id="tools" :style= "[tool ? { 'visibility': 'visible' } : { 'visibility': 'hidden' }]"> 
