d1e4142d4a19cdaa63e92dd3c50f5a6c6b1fd717
Steven Kight <sgk0711@gmail.com>
2026-03-04 20:16:34 -0500

    Add new skills and languages learned to the skills section

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -35,7 +35,9 @@ const SkillsList = {
         { img_path: '/assets/images/logos/keras-svgrepo-com.svg', title: 'Keras', ref_link: 'https://keras.io/' },
         { img_path: '/assets/images/logos/scikit-learn.svg', title: 'Scikit', ref_link: 'https://scikit-learn.org/' },
         { img_path: '/assets/images/logos/pandas-svgrepo-com.svg', title: 'Pandas', ref_link: 'https://pandas.pydata.org/' },
-        { img_path: '/assets/images/logos/django-svgrepo-com.svg', title: 'Django', ref_link: 'https://www.djangoproject.com/' }
+        { img_path: '/assets/images/logos/django-svgrepo-com.svg', title: 'Django', ref_link: 'https://www.djangoproject.com/' },
+        { img_path: '/assets/images/logos/fastapi.svg', title: 'FastAPI', ref_link: 'https://fastapi.tiangolo.com/' },
+        { img_path: '/assets/images/logos/langchain.svg', title: 'LangChain', ref_link: 'https://www.langchain.com/' },
     ],
     
     'Operating Systems': [
